6978d722224ea8bdd6025a1ee7e988b665c9a819
Saki Yoshida <saki.yoshida.biz@gmail.com>
2022-06-16 23:51:51 +0900

    feature:#10 親ホーム作成

---
+++ src/views/ParentHome.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="parent_home">
+    <div>
+      お店一覧コンポーネント
+    </div>
+    <div>
+      商品一覧コンポーネント
+    </div>
+    <!-- <div>
+      お小遣い管理
+    </div> -->
+    <div>
+      子供登録
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "ParentHome",
+  components: {
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
